9253c61eee48ca14eabe5095df2ebf449ab34421
Zeeshan <zeetherocker@gmail.com>
2015-09-29 21:54:57 +0530

    Add Event titles and time

--- app/src/main/java/com/zap/Kalanjali/EventMasterFlow/EventList1.java
+++ app/src/main/java/com/zap/Kalanjali/EventMasterFlow/EventList1.java
@@ -18,11 +18,16 @@ public class EventList1 {
         addItem(new EventItem(R.string.day1_event3, R.drawable.ic_card1, R.string.day1_desc3, R.string.day1_time3, 2));
         addItem(new EventItem(R.string.day1_event4, R.drawable.ic_card1, R.string.day1_desc4, R.string.day1_time4, 3));
         addItem(new EventItem(R.string.day1_event5, R.drawable.ic_card1, R.string.day1_desc5, R.string.day1_time5, 4));
-        addItem(new EventItem(R.string.day1_event6, R.drawable.ic_card1, R.string.day1_desc6, R.string.day1_time6, 5));
+        addItem(new EventItem(R.string.day1_event6, R.drawable.ic_card1, R.string.day1_desc6, R.string.day1_time6, 1));
         addItem(new EventItem(R.string.day1_event7, R.drawable.ic_card1, R.string.day1_desc7, R.string.day1_time7, 2));
         addItem(new EventItem(R.string.day1_event8, R.drawable.ic_card1, R.string.day1_desc8, R.string.day1_time8, 1));
         addItem(new EventItem(R.string.day1_event9, R.drawable.ic_card1, R.string.day1_desc9, R.string.day1_time9, 3));
         addItem(new EventItem(R.string.day1_event10, R.drawable.ic_card1, R.string.day1_desc10, R.string.day1_time10, 5));
+        addItem(new EventItem(R.string.day1_event11, R.drawable.ic_card1, R.string.day1_desc11, R.string.day1_time11, 5));
+        addItem(new EventItem(R.string.day1_event12, R.drawable.ic_card1, R.string.day1_desc12, R.string.day1_time12, 5));
+        addItem(new EventItem(R.string.day1_event13, R.drawable.ic_card1, R.string.day1_desc13, R.string.day1_time13, 1));
+        addItem(new EventItem(R.string.day1_event14, R.drawable.ic_card1, R.string.day1_desc14, R.string.day1_time14, 5));
+        addItem(new EventItem(R.string.day1_event15, R.drawable.ic_card1, R.string.day1_desc15, R.string.day1_time15, 5));
     }
 
     private static void addItem(EventItem item) {
--- app/src/main/java/com/zap/Kalanjali/EventMasterFlow/EventList2.java
+++ app/src/main/java/com/zap/Kalanjali/EventMasterFlow/EventList2.java
@@ -28,9 +28,19 @@ public class EventList2 extends Fragment {
         addItem(new EventItem(R.string.day2_event5, R.drawable.ic_card1, R.string.day2_desc5, R.string.day2_time5, 6));
         addItem(new EventItem(R.string.day2_event6, R.drawable.ic_card1, R.string.day2_desc6, R.string.day2_time6 ,1));
         addItem(new EventItem(R.string.day2_event7, R.drawable.ic_card1, R.string.day2_desc7, R.string.day2_time7, 2));
-        addItem(new EventItem(R.string.day2_event8, R.drawable.ic_card1, R.string.day2_desc8, R.string.day2_time8, 3));
+        addItem(new EventItem(R.string.day2_event8, R.drawable.ic_card1, R.string.day2_desc8, R.string.day2_time8, 1));
         addItem(new EventItem(R.string.day2_event9, R.drawable.ic_card1, R.string.day2_desc9, R.string.day2_time9, 4));
         addItem(new EventItem(R.string.day2_event10, R.drawable.ic_card1, R.string.day2_desc10, R.string.day2_time10, 5));
+        addItem(new EventItem(R.string.day2_event11, R.drawable.ic_card1, R.string.day2_desc11, R.string.day2_time11, 5));
+        addItem(new EventItem(R.string.day2_event12, R.drawable.ic_card1, R.string.day2_desc12, R.string.day2_time12, 5));
+        addItem(new EventItem(R.string.day2_event13, R.drawable.ic_card1, R.string.day2_desc13, R.string.day2_time13, 5));
+        addItem(new EventItem(R.string.day2_event14, R.drawable.ic_card1, R.string.day2_desc14, R.string.day2_time14, 5));
+        addItem(new EventItem(R.string.day2_event15, R.drawable.ic_card1, R.string.day2_desc15, R.string.day2_time15, 5));
+        addItem(new EventItem(R.string.day2_event16, R.drawable.ic_card1, R.string.day2_desc16, R.string.day2_time16, 5));
+        addItem(new EventItem(R.string.day2_event17, R.drawable.ic_card1, R.string.day2_desc17, R.string.day2_time17, 5));
+        addItem(new EventItem(R.string.day2_event18, R.drawable.ic_card1, R.string.day2_desc18, R.string.day2_time18, 5));
+        addItem(new EventItem(R.string.day2_event19, R.drawable.ic_card1, R.string.day2_desc19, R.string.day2_time19, 5));
+        addItem(new EventItem(R.string.day2_event20, R.drawable.ic_card1, R.string.day2_desc20, R.string.day2_time20, 5));
     }
 
     private static void addItem(EventItem item) {
