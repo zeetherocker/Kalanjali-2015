46bf3a92dce7b3bcb403ca6a8f7e499fb04eaa26
Zeeshan <zeetherocker@gmail.com>
2015-10-01 18:06:00 +0530

    Add all event descriptions (Part-2)

--- app/src/main/java/com/zap/Kalanjali/EventMasterFlow/EventList1.java
+++ app/src/main/java/com/zap/Kalanjali/EventMasterFlow/EventList1.java
@@ -28,11 +28,14 @@ public class EventList1 {
         addItem(new EventItem(R.string.day1_event13, R.drawable.ic_card1, R.string.day1_desc13, R.string.day1_time13, 1));
         addItem(new EventItem(R.string.day1_event14, R.drawable.ic_card1, R.string.day1_desc14, R.string.day1_time14, 1));
         addItem(new EventItem(R.string.day1_event15, R.drawable.ic_card1, R.string.day1_desc15, R.string.day1_time15, 9));
-        addItem(new EventItem(R.string.day1_event16, R.drawable.ic_card1, R.string.day1_desc16, R.string.day1_time16, 1));
+        addItem(new EventItem(R.string.day1_event16, R.drawable.ic_card1, R.string.day1_desc16, R.string.day1_time16, 5));
         addItem(new EventItem(R.string.day1_event17, R.drawable.ic_card1, R.string.day1_desc17, R.string.day1_time17, 1));
         addItem(new EventItem(R.string.day1_event18, R.drawable.ic_card1, R.string.day1_desc18, R.string.day1_time18, 10));
-        addItem(new EventItem(R.string.day1_event19, R.drawable.ic_card1, R.string.day1_desc19, R.string.day1_time19, 5));
+        addItem(new EventItem(R.string.day1_event19, R.drawable.ic_card1, R.string.day1_desc19, R.string.day1_time19, 1));
         addItem(new EventItem(R.string.day1_event20, R.drawable.ic_card1, R.string.day1_desc20, R.string.day1_time20, 1));
+        addItem(new EventItem(R.string.day1_event21, R.drawable.ic_card1, R.string.day1_desc21, R.string.day1_time21, 1));
+        addItem(new EventItem(R.string.day1_event22, R.drawable.ic_card1, R.string.day1_desc22, R.string.day1_time22, 1));
+        addItem(new EventItem(R.string.day1_event23, R.drawable.ic_card1, R.string.day1_desc23, R.string.day1_time23, 1));
     }
 
     private static void addItem(EventItem item) {
