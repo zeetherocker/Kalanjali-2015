636dbc4f49172f6f6e7074f2184f0ad274b6f957
Zeeshan <zeetherocker@gmail.com>
2015-10-11 00:43:56 +0530

    Minor Changes

--- app/src/main/java/com/zap/Kalanjali/FirstFragment.java
+++ app/src/main/java/com/zap/Kalanjali/FirstFragment.java
@@ -27,28 +27,6 @@ public class FirstFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         myView = inflater.inflate(R.layout.fragment_first, container, false);
-
-        cardView = (RecyclerView) myView.findViewById(R.id.card_list);
-        cardAdapter = new CardAdapter(getData());
-        cardView.setAdapter(cardAdapter);
-        cardView.setLayoutManager(new LinearLayoutManager(getActivity()));
-
-        layoutManager = new LinearLayoutManager(getActivity());                 // Creating a layout Manager
-
-        cardView.setLayoutManager(layoutManager);
         return myView;
-    }
-
-    public static List<CardInfo> getData() {
-        List<CardInfo> data = new ArrayList<>();
-        int[] icons = {R.drawable.ic_card1, R.drawable.ic_card2};
-        String[] titles = {"Event 1" ,"Event 2"};
-        for(int i=0; i<icons.length && i<titles.length; i++){
-            CardInfo current = new CardInfo();
-            current.iconId = icons[i];
-            current.title = titles[i];
-            data.add(current);
-        }
-        return data;
     }
 }
